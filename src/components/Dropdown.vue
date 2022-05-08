0f7a75b629372979bcf9695c4a642c94620f4093
Hacker-C <1507559148@qq.com>
2022-05-08 14:21:11 +0800

    ✨ 下拉菜单完成

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="dropdown">
+    <button
+      class="btn btn-primary border dropdown-toggle"
+      type="button"
+      id="dropdownMenuButton1"
+      data-bs-toggle="dropdown"
+      aria-expanded="false"
+      @click="onToggle"
+    >
+      {{ title }}
+    </button>
+    <ul
+      class="dropdown-menu"
+      :style="{ display: isOpen ? 'block' : 'none' }"
+      aria-labelledby="dropdownMenuButton1"
+    >
+      <slot></slot>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'DropDown',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    // FEAT 点击开启关闭下拉菜单
+    const isOpen = ref(false)
+    const onToggle = () => {
+      isOpen.value = !isOpen.value
+    }
+    return {
+      isOpen,
+      onToggle,
+    }
+  },
+})
+</script>
