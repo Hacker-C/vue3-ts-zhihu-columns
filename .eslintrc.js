9c65d8fd78de49bf008ab0d94560ab516431fcb8
Hacker-C <1507559148@qq.com>
2022-05-08 16:20:52 +0800

    🔧 删除无用配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,8 +16,5 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    quotes: [1, 'single'], // 引号类型 `` "" ''
-    semi: [2, 'never'], // 语句强制分号结尾
-    'space-before-function-paren': [0, 'always'], //函数定义时括号前面要不要有空格
   },
 }
