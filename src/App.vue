ffde2b3caa76a6b374ea789884c2917e95b57459
Hacker-C <1507559148@qq.com>
2022-05-08 12:01:37 +0800

    ✨ 完成专栏项展示

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,53 @@
-<template>hello world</template>
+<template>
+  <div class="container">
+    <ColumnList :list="testData" />
+  </div>
+</template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: '专栏测试test1',
+    description: '这是test1专栏，介绍vue3的的各种特性，非常有意思',
+    avatar:
+      'https://cdn.jsdelivr.net/gh/Hacker-C/Picture-Bed@main/icons/vuejs.png',
+  },
+  {
+    id: 2,
+    title: '专栏测试test1',
+    description: '这是test1专栏，介绍vue3的的各种特性，非常有意思',
+    avatar:
+      'https://cdn.jsdelivr.net/gh/Hacker-C/Picture-Bed@main/icons/vuejs.png',
+  },
+  {
+    id: 3,
+    title: '专栏测试test1',
+    description: '这是test1专栏，介绍vue3的的各种特性，非常有意思',
+    avatar:
+      'https://cdn.jsdelivr.net/gh/Hacker-C/Picture-Bed@main/icons/vuejs.png',
+  },
+  {
+    id: 4,
+    title: '专栏测试test1',
+    description: '这是test1专栏，介绍vue3的的各种特性，非常有意思',
+  },
+]
+
 export default defineComponent({
   name: 'App',
+  components: {
+    ColumnList,
+  },
+  setup() {
+    return {
+      testData,
+    }
+  },
 })
 </script>
 
--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,10 +1,56 @@
-<template></template>
+<template>
+  <div class="row">
+    <div class="col-3 mb-3" v-for="column in finalColumnList" :key="column.id">
+      <div class="card h-100 shadow-sm">
+        <div class="text-center">
+          <img
+            :src="column.avatar"
+            :alt="column.title"
+            class="rounded-circle border w-25 my-3"
+          />
+        </div>
+        <div class="card-body text-center">
+          <h5 class="card-title">{{ column.title }}</h5>
+          <p class="card-text text-left">
+            {{ column.description }}
+          </p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType, computed } from 'vue'
+
+export interface ColumnProps {
+  id: number
+  title: string
+  avatar?: string
+  description: string
+}
 
 export default defineComponent({
-  name: 'App',
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+    },
+  },
+  setup(props) {
+    const finalColumnList = computed(() => {
+      return props.list?.map(column => {
+        if (!column.avatar) {
+          column.avatar = require('@/assets/column.png')
+        }
+        return column
+      })
+    })
+    return {
+      finalColumnList,
+    }
+  },
 })
 </script>
 
