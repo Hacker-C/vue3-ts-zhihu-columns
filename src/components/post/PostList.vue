d4e6b9f1154423e0ebda0a7cbb0de43f669d20ce
Hacker-C <1507559148@qq.com>
2022-05-10 22:53:38 +0800

    💄 优化显示

--- src/components/post/PostList.vue
+++ src/components/post/PostList.vue
@@ -7,8 +7,8 @@
     >
       <h4>{{ post.title }}</h4>
       <div class="row my-3 align-items-center">
-        <div class="col-3" v-if="post.image">
-          <img :src="post.image" :alt="post.title" class="rounded-lg w-25" />
+        <div class="col-3 text-center" v-if="post.image">
+          <img :src="post.image" :alt="post.title" class="rounded-lg w-50" />
         </div>
         <p :class="{ 'col-9': post.image }">{{ post.content }}</p>
       </div>
