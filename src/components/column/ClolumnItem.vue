4909dd6923b8f656447fbcaa9dfa773630d1fac7
Hacker-C <1507559148@qq.com>
2022-05-10 17:07:30 +0800

    ♻️ 重构 column 组件

---
+++ src/components/column/ClolumnItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="col-3 mb-3" :key="column.id">
+    <div class="card h-100 shadow-sm">
+      <div class="text-center">
+        <img
+          :src="column.avatar"
+          :alt="column.title"
+          class="rounded-circle border w-25 my-3"
+        />
+      </div>
+      <div class="card-body text-center">
+        <h5 class="card-title">{{ column.title }}</h5>
+        <p class="card-text text-left">
+          {{ column.description }}
+        </p>
+        <a href="#" class="btn btn-outline-primary">进入专栏</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { ColumnProps } from './ColumnList.vue'
+
+export default defineComponent({
+  name: 'ClolumnItem',
+  props: {
+    column: {
+      type: Object as PropType<ColumnProps>,
+      required: true,
+    },
+  },
+})
+</script>
---
+++ src/components/column/ColumnList.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="row">
+    <ClolumnItem
+      v-for="item in finalColumnList"
+      :column="item"
+      :key="item.id"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+import ClolumnItem from './ClolumnItem.vue'
+
+export interface ColumnProps {
+  id: number
+  title: string
+  avatar?: string
+  description: string
+}
+
+export default defineComponent({
+  name: 'ColumnList',
+  components: {
+    ClolumnItem,
+  },
+  setup() {
+    const testData: ColumnProps[] = [
+      {
+        id: 1,
+        title: '专栏测试test1',
+        description: '这是test1专栏，介绍vue3的的各种特性，非常有意思',
+        avatar:
+          'https://cdn.jsdelivr.net/gh/Hacker-C/Picture-Bed@main/icons/vuejs.png',
+      },
+      {
+        id: 2,
+        title: '专栏测试test1',
+        description: '这是test1专栏，介绍vue3的的各种特性，非常有意思',
+        avatar:
+          'https://cdn.jsdelivr.net/gh/Hacker-C/Picture-Bed@main/icons/vuejs.png',
+      },
+      {
+        id: 3,
+        title: '专栏测试test1',
+        description: '这是test1专栏，介绍vue3的的各种特性，非常有意思',
+        avatar:
+          'https://cdn.jsdelivr.net/gh/Hacker-C/Picture-Bed@main/icons/vuejs.png',
+      },
+      {
+        id: 4,
+        title: '专栏测试test1',
+        description: '这是test1专栏，介绍vue3的的各种特性，非常有意思',
+      },
+    ]
+    const finalColumnList = computed(() => {
+      return testData?.map(column => {
+        if (!column.avatar) {
+          column.avatar = require('@/assets/column.png')
+        }
+        return column
+      })
+    })
+    return {
+      finalColumnList,
+    }
+  },
+})
+</script>
+
+<style scoped></style>
